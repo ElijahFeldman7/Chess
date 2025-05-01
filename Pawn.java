07306ec888adca2c0a8df8be697acf3e2e756b64
TeomanEkici <teoekici@gmail.com>
2025-05-01 12:51:01 -0400

    Pawn

---
+++ Pawn.java
@@ -0,0 +1,25 @@
+public class Pawn implements Piece {
+    private String position;
+    private String type = "Pawn";
+
+    public Pawn(String position) {
+        this.position = position;
+        this.type = "Pawn";
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public void move(Square[][] board, String newPosition) {
+        this.position = newPosition;
+    }
+    
+}
